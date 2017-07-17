7f5733ee86cf80f3ffaf85208a341a6ebef9bd33
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-07-17 20:26:26 -0300

    Atualizações na conexão com o BD

--- Geral-core/src/br/cefetmg/inf/util/db/MySQLConnection.java
+++ Geral-core/src/br/cefetmg/inf/util/db/MySQLConnection.java
@@ -9,9 +9,10 @@ import java.util.logging.Logger;
 public class MySQLConnection implements ConnectionFactory {
 
     private final static String dbDriver = "com.mysql.jdbc.Driver";
-    private final static String dbURL = "jdbc:postgresql://localhost:5432/sislocdb";
-    private final static String user = "postgres";
-    private final static String pass = "postgres";
+    private final static String dbURL = "jdbc:mysql://localhost:3306/geral";
+    //private final static String dbURL = "jdbc:mysql://http://localhost/phpmyadmin/db_structure.php?server=1&db=geral&token=002f17d7d91a113c2994de22676097b2";
+    private final static String user = "";
+    private final static String pass = "";
 
     public MySQLConnection() {
     }
