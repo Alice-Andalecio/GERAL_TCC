da474e76f2d7bd605a6ee404006b16b5d7277b5e
Luana Pinheiro <pinheiro.luana38@outlook.com>
2017-09-28 11:29:20 -0300

    Add camda de visão
    Adiciona a pasta de visão junto com o css

---
+++ Geral-core/src/br/cefetmg/inf/geral/view/FXMLController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.inf.geral.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Aluno
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
