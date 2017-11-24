789d39fce1df37af858164ea22b0470877b92a0a
Gabriel Victor <gabrielvgx@gmail.com>
2017-11-24 00:53:44 -0200

    Corrigindo Bug Animais.js

--- GeralViewController/web/js/Animais.js
+++ GeralViewController/web/js/Animais.js
@@ -5,20 +5,20 @@ window.onload = function () {
 };
 
 function CriaId() {
-    var numAnimal = document.querySelector('#tdNumAnimal').innerHTML;
-    
-    var input = document.createElement('input');
-    var label = document.createElement('label');
-
-    input.setAttribute('type', 'radio');
-    input.setAttribute('name', 'select' + numAnimal);
-    input.setAttribute('id', 'select' + numAnimal);
-    input.classList.add('with-gap');
-    
-    label.setAttribute('for', 'select' + numAnimal);
-    
-    document.querySelector('#tdSelect').appendChild(input);
-    document.querySelector('#tdSelect').appendChild(label);
+    var x = document.querySelectorAll('#tdSelect');
+        var numAnimal = document.querySelectorAll('#tdNumAnimal');
+    for (var i = 0; i < x.length; i++) {
+        var input = document.createElement('input');
+        var label = document.createElement('label');
+        input.setAttribute('type', 'radio');
+        input.setAttribute('name', 'select' + numAnimal[i].innerHTML);
+        input.setAttribute('id', 'select' + numAnimal[i].innerHTML);
+        input.classList.add('with-gap');
+        label.setAttribute('for', 'select' + numAnimal[i].innerHTML);
+        
+        x[i].appendChild(input);
+        x[i].appendChild(label);
+    }
 }
 
 function MostrarFichaAnimal() {
